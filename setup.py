cb448d4e4310eb89f568bcc427de5cad917a201a
Damian <damian.peter.shaw@gmail.com>
2021-07-18 01:39:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ if sys.version_info < (3, 9):
     sys.exit('Python 3.9 or higher is required')
 
 setup(
-    name='Fall Guys Ping Extimator',
+    name='Fall Guys Ping Estimate',
     version='0.0.1',
     description='Python Distribution Utilities',
     author='@notatallshaw',
